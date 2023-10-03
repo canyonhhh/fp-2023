c462d873ca998befb76411d5612413ad252e06a3
Joris <jorislisas@gmail.com>
2023-10-03 09:47:15 +0300

    requirements

--- src/Lib2.hs
+++ src/Lib2.hs
@@ -1,5 +1,23 @@
 {-# OPTIONS_GHC -Wno-unused-top-binds #-}
 
+-- SHOW TABLES
+-- SHOW TABLE name
+-- Parse select statemets with:
+-- max
+   -- Parse the MAX aggregate function.
+   -- Return the largest value in the specified column.
+   -- Ensure that only integers, bools, and strings are processed.
+-- sum
+   -- Parse the SUM aggregate function.
+   -- Return the sum of all values in the specified column.
+   -- Ensure that integers are processed.
+-- where AND
+   -- Parse multiple conditions combined using AND.
+   -- Ensure all conditions combined with AND are met for a row are included in the result.
+   -- Aggregate functions can be applied to the results ( MIN, MAX, etc.)
+-- where bool is true/false
+   -- Filter rows based on whether the specified column's value is TRUE or FALSE.
+
 module Lib2
   ( parseStatement,
     executeStatement,
