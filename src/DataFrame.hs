b9a907edc1b18539b6f363b54cd21e1cb75952a8
Joris <jorislisas@gmail.com>
2023-11-14 10:32:37 +0200

    DSL in Free Monad and serialization setup

--- src/DataFrame.hs
+++ src/DataFrame.hs
@@ -1,22 +1,38 @@
-module DataFrame (Column (..), ColumnType (..), Value (..), Row, DataFrame (..)) where
+{-# LANGUAGE DeriveGeneric #-}
+module DataFrame (Column (..), ColumnType (..), DataFrame.Value (..), Row, DataFrame (..)) where
+
+import GHC.Generics (Generic)
+import Data.Aeson
 
 data ColumnType
   = IntegerType
   | StringType
   | BoolType
-  deriving (Show, Eq)
+  deriving (Show, Eq, Generic)
+
+instance ToJSON ColumnType
+instance FromJSON ColumnType
 
 data Column = Column String ColumnType
-  deriving (Show, Eq)
+  deriving (Show, Eq, Generic)
+
+instance ToJSON Column
+instance FromJSON Column
 
 data Value
   = IntegerValue Integer
   | StringValue String
   | BoolValue Bool
   | NullValue
-  deriving (Show, Eq)
+  deriving (Show, Eq, Generic)
 
-type Row = [Value]
+instance ToJSON DataFrame.Value
+instance FromJSON DataFrame.Value
+
+type Row = [DataFrame.Value]
 
 data DataFrame = DataFrame [Column] [Row]
-  deriving (Show, Eq)
+  deriving (Show, Eq, Generic)
+
+instance ToJSON DataFrame
+instance FromJSON DataFrame
--- src/Lib3.hs
+++ src/Lib3.hs
@@ -2,14 +2,12 @@
 {-# OPTIONS_GHC -Wno-unused-top-binds #-}
 {-# OPTIONS_GHC -Wno-name-shadowing #-}
 {-# OPTIONS_GHC -Wno-partial-fields #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Lib3
   ( executeSql,
     Execution,
     ExecutionAlgebra(..),
-    insertInto,
-    updateTable,
-    deleteFrom
   )
 where
 
@@ -17,6 +15,8 @@ import Control.Monad.Free (Free (..), liftF)
 import DataFrame
 import Lib2
 import Data.Time (UTCTime)
+import Data.Yaml (decodeEither', encode)
+import qualified Data.ByteString as BS
 
 type TableName = String
 type FileContent = String
@@ -25,9 +25,19 @@ type ErrorMessage = String
 data ExecutionAlgebra next
   = LoadFile TableName (FileContent -> next)
   | GetTime (UTCTime -> next)
-  | InsertInto TableName Row (Either ErrorMessage () -> next)
-  | UpdateTable TableName [(String, Value)] (Maybe Condition) (Either ErrorMessage () -> next)
+  | ParseDataFrame FileContent (DataFrame -> next)
+  | FilterRows (Maybe Condition) DataFrame (DataFrame -> next)
+  | JoinTables [(Aggregate, Maybe TableName, Cname)] (Maybe Condition) DataFrame DataFrame (DataFrame -> next)
+  | InsertInto DataFrame Row (DataFrame -> next)
   | DeleteFrom TableName (Maybe Condition) (Either ErrorMessage () -> next)
+  | ShowTables (Either ErrorMessage [TableName] -> next)
+  | ShowTableStructure TableName (Either ErrorMessage DataFrame -> next)
+  | SaveTableData TableName DataFrame (Either ErrorMessage () -> next)
+  | AggregateData [(Aggregate, Maybe TableName, Cname)] DataFrame (DataFrame -> next)
+  | ReportError ErrorMessage next
+  | SelectColumns [(Aggregate, Maybe TableName, Cname)] DataFrame (DataFrame -> next)
+  | ParseRow [Value] (Row -> next)
+  | UpdateTableDataFrame DataFrame [(Cname, Value)] (Maybe Condition) (DataFrame -> next)
   deriving Functor
 
 type Execution = Free ExecutionAlgebra
@@ -38,30 +48,97 @@ loadFile name = liftF $ LoadFile name id
 getTime :: Execution UTCTime
 getTime = liftF $ GetTime id
 
-insertInto :: TableName -> [Value] -> Execution (Either ErrorMessage ())
-insertInto tableName values = liftF $ InsertInto tableName values id
+parseDataFrame :: FileContent -> Execution DataFrame
+parseDataFrame content = liftF $ ParseDataFrame content id
 
-updateTable :: TableName -> [(String, Value)] -> Maybe Condition -> Execution (Either ErrorMessage ())
-updateTable tableName values whereClause = liftF $ UpdateTable tableName values whereClause id
+filterRows :: Maybe Condition -> DataFrame -> Execution DataFrame
+filterRows condition df = liftF $ FilterRows condition df id
 
-deleteFrom :: TableName -> Maybe Condition -> Execution (Either ErrorMessage ())
-deleteFrom tableName whereClause = liftF $ DeleteFrom tableName whereClause id
+aggregateData :: [(Aggregate, Maybe TableName, Cname)] -> DataFrame -> Execution DataFrame
+aggregateData columns df = liftF $ AggregateData columns df id
+
+selectColumns :: [(Aggregate, Maybe TableName, Cname)] -> DataFrame -> Execution DataFrame
+selectColumns columns df = liftF $ SelectColumns columns df id
+
+joinTables :: [(Aggregate, Maybe TableName, Cname)] -> Maybe Condition -> DataFrame -> DataFrame -> Execution DataFrame
+joinTables columns cond df1 df2 = liftF $ JoinTables columns cond df1 df2 id
+
+insertInto :: DataFrame -> Row -> Execution DataFrame
+insertInto df row = liftF $ InsertInto df row id
+
+saveTableData :: TableName -> DataFrame -> Execution (Either ErrorMessage ())
+saveTableData tableName df = liftF $ SaveTableData tableName df id
+
+parseRow :: [Value] -> Execution Row
+parseRow values = liftF $ ParseRow values id
+
+updateTable :: DataFrame -> [(Cname, Value)] -> Maybe Condition -> Execution DataFrame
+updateTable df updates condition = liftF $ UpdateTableDataFrame df updates condition id
+
+-- Function to serialize a DataFrame to YAML
+serializeDataFrame :: DataFrame -> BS.ByteString
+serializeDataFrame = encode
+
+-- Function to deserialize YAML to a DataFrame
+deserializeDataFrame :: BS.ByteString -> Either String DataFrame
+deserializeDataFrame bs = case decodeEither' bs of
+    Left err -> Left (show err)
+    Right df -> Right df
 
 -- Placeholder function to execute SQL commands
 executeSql :: String -> Execution (Either ErrorMessage DataFrame)
 executeSql sql = case parseStatement sql of
     Left errMsg -> return $ Left errMsg
     Right stmt -> case stmt of
-        Insert tableName values -> executeInsert tableName values
-        Update tableName values whereClause -> executeUpdate tableName values whereClause
-        Delete tableName whereClause -> executeDelete tableName whereClause
-        _ -> return $ Left "Unsupported statement"
-
-executeInsert :: TableName -> [Value] -> Execution (Either ErrorMessage DataFrame)
-executeInsert _ _ = return $ Left "Not implemented"
-
-executeUpdate :: TableName -> [(String, Value)] -> Maybe Condition -> Execution (Either ErrorMessage DataFrame)
-executeUpdate _ _ _ = return $ Left "Not implemented"
-
-executeDelete :: TableName -> Maybe Condition -> Execution (Either ErrorMessage DataFrame)
-executeDelete _ _ = return $ Left "Not implemented"
+        ShowTable tableName -> do
+            fileContent <- loadFile tableName
+            df <- parseDataFrame fileContent
+            return $ Right df
+        Lib2.ShowTables -> do
+            fileContent <- loadFile "tables.yaml"
+            df <- parseDataFrame fileContent
+            return $ Right df
+        SelectAll tableName -> do
+            fileContent <- loadFile tableName
+            df <- parseDataFrame fileContent
+            return $ Right df
+        Select columms tableNames whereClause -> do
+            case length tableNames of
+                1 -> do
+                    fileContent <- loadFile (head tableNames)
+                    df <- parseDataFrame fileContent
+                    filteredDf <- filterRows whereClause df
+                    aggregateDf <- aggregateData (columns stmt) filteredDf
+                    selectedDf <- Lib3.selectColumns (columns stmt) aggregateDf
+                    return $ Right selectedDf
+                2 -> do
+                    fileContent1 <- loadFile (head tableNames)
+                    fileContent2 <- loadFile (last tableNames)
+                    df1 <- parseDataFrame fileContent1
+                    df2 <- parseDataFrame fileContent2
+                    filteredDf1 <- filterRows whereClause df1
+                    filteredDf2 <- filterRows whereClause df2
+                    aggregateDf1 <- aggregateData (columns stmt) filteredDf1
+                    aggregateDf2 <- aggregateData (columns stmt) filteredDf2
+                    selectedDf <- joinTables (columns stmt) whereClause aggregateDf1 aggregateDf2
+                    return $ Right selectedDf
+                _ -> return $ Left "Joining more than 2 tables is not supported"
+        Insert tableName values -> do
+            fileContent <- loadFile tableName
+            df <- parseDataFrame fileContent
+            newRow <- parseRow values
+            newDf <- insertInto df newRow
+            _ <- saveTableData tableName newDf
+            return $ Right newDf
+        Update tableName values whereClause -> do
+            fileContent <- loadFile tableName
+            df <- parseDataFrame fileContent
+            updatedDf <- updateTable df values whereClause
+            _ <- saveTableData tableName updatedDf
+            return $ Right updatedDf
+        Delete tableName whereClause -> do
+            fileContent <- loadFile tableName
+            df <- parseDataFrame fileContent
+            updatedDf <- filterRows whereClause df
+            _ <- saveTableData tableName updatedDf
+            return $ Right $ DataFrame [] []
