3e2387d0a8218311fb47ec94760e17793921bbcb
fungcinis <145013187+fungcinis@users.noreply.github.com>
2023-11-20 17:30:08 +0200

    Update Main.hs

--- app3/Main.hs
+++ app3/Main.hs
@@ -1,14 +1,23 @@
+{-# OPTIONS_GHC -Wno-unused-top-binds #-}
+{-# OPTIONS_GHC -Wno-name-shadowing #-}
+{-# OPTIONS_GHC -Wno-partial-fields #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main (main) where
 
 import Control.Monad.IO.Class (MonadIO (liftIO))
 import Control.Monad.Free (Free (..))
 
-import Data.Functor((<&>))
-import Data.Time ( UTCTime, getCurrentTime )
+import Data.Time ( getCurrentTime)
+import System.Directory ( doesFileExist, listDirectory )
+import System.FilePath (takeBaseName, takeExtension)
+import System.IO.Error (catchIOError)
 import Data.List qualified as L
 import Lib1 qualified
 import Lib2 qualified
-import Lib3 qualified
+import Lib3
+import DataFrame
+import Util
 import System.Console.Repline
   ( CompleterStyle (Word),
     ExitDecision (Exit),
@@ -59,10 +68,63 @@ main =
 
 runExecuteIO :: Lib3.Execution r -> IO r
 runExecuteIO (Pure r) = return r
-runExecuteIO (Free step) = do
-    next <- runStep step
-    runExecuteIO next
-    where
-        -- probably you will want to extend the interpreter
-        runStep :: Lib3.ExecutionAlgebra a -> IO a
-        runStep (Lib3.GetTime next) = getCurrentTime >>= return . next
+runExecuteIO (Free step) = runStep step >>= runExecuteIO
+
+runStep :: Lib3.ExecutionAlgebra a -> IO a
+runStep (Lib3.LoadFile tableName next) = do
+    fileResult <- readFileIO tableName
+    return $ next fileResult
+runStep (Lib3.ParseDataFrame fileContent next) =
+    return $ next (Lib3.deserializeDataFrame fileContent)
+runStep (Lib3.GetTime next) =
+    do next <$> getCurrentTime
+runStep (Lib3.SaveTableData tableName dataframe next) = 
+    case Lib3.serializeDataFrame dataframe of
+        Left errMsg -> return $ next $ Left errMsg
+        Right serializedData -> do
+            writeFileIO tableName serializedData
+            return $ next $ Right ()
+runStep (Lib3.FilterRows maybeCondition dataframe next) = do
+    return $ next $ Lib2.applyWhereClauses maybeCondition dataframe
+runStep (Lib3.JoinTables maybeCondition df1 df2 next) = do
+    return $ next $ joinTables maybeCondition df1 df2
+runStep (Lib3.InsertInto values dataframe next) = do
+    return $ next $ insertInto values dataframe
+runStep (Lib3.AggregateData aggregates dataframe next) = do
+    return $ next $ Lib2.applyAggregates aggregates dataframe
+runStep (Lib3.SelectColumns aggregates dataframe next) = do
+    return $ next $ Lib2.selectColumns aggregates dataframe
+runStep (Lib3.UpdateTableDataFrame dataframe updates maybeCondition next) = do
+    return$ next $ updateTableDataFrame dataframe updates maybeCondition
+runStep (Lib3.ReportError err next) = do
+    return $ error ("shits broken: " ++ err)
+runStep (Lib3.GetTableNames next) = do
+    next <$> getTableNames
+
+reportError :: String -> DataFrame
+reportError err = DataFrame [Column "error" StringType] [[StringValue err]]
+
+-- check if file exists
+readFileIO :: TableName -> IO (Either ErrorMessage FileContent)
+readFileIO tableName = do
+    let fileName = "db/" ++ tableName ++ ".yaml"
+    fileExists <- doesFileExist fileName
+    if fileExists
+        then Right <$> readFile fileName
+        else return $ Left $ "Table " ++ tableName ++ " does not exist."
+
+writeFileIO :: TableName -> FileContent -> IO ()
+writeFileIO tableName fileContent = do
+    let fileName = "db/" ++ tableName ++ ".yaml"
+    writeFile fileName fileContent
+
+getTableNames :: IO [TableName]
+getTableNames = catchIOError listTableNames handleIOError
+  where
+    listTableNames = do
+      files <- listDirectory "db"
+      let yamlFiles = filter (\f -> takeExtension f == ".yaml") files
+      return $ map takeBaseName yamlFiles
+
+    handleIOError :: IOError -> IO [TableName]
+    handleIOError _ = return []
